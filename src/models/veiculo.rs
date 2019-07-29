0e6d0ac99f567074498c932797463314a5b32552
Rodrigo <rodrigomauricio@protonmail.com>
2019-07-28 21:16:43 -0300

    Concertado bug

--- src/models/veiculo.rs
+++ src/models/veiculo.rs
@@ -15,7 +15,7 @@ impl Veiculo {
         self.relatorios.sort_by(|a, b| a.timestamp.partial_cmp(&b.timestamp).unwrap());
         let mut prev_timestamp: i64 = 0;
         let mut prev_km: i64 = 0;
-        for r in self.relatorios.iter() {
+        for r in self.relatorios.iter_mut() {
             if r.timestamp.parse::<i64>().unwrap() < prev_timestamp {
                 panic!("Erro na validação das timestamps do veiculo")
             } else {
@@ -24,9 +24,11 @@ impl Veiculo {
                 } else {
                     prev_timestamp = r.timestamp.parse::<i64>().unwrap();
                     prev_km = r.km.clone();
+                    self.km_atual = prev_km.clone();
                 }
             }
         }
+
 
         println!("Sucesso na validação do veiculo!");
     }
